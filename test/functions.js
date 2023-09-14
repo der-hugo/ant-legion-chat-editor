b9175748ff780ec1d62d96abeff68c16fa0d343d
Jerome Pönisch <jerome.poenisch@unity3d.com>
2023-09-14 17:44:52 +0200

    asd

--- test/functions.js
+++ test/functions.js
@@ -5,186 +5,6 @@ const color = document.getElementById("color");
 const exampleTemplate = 'This is an <font color="#ff0000">EXAMPLE</font>! 😀';
 const defaultColor = "ffffc8";
 
-function Init() {
-    input.innerHTML = exampleTemplate;
-
-    color.value = "#" + defaultColor;
-
-    OnInputChanged();
-}
-
-function SetTemplate() {
-    document.getElementById("input").innerHTML = event.currentTarget.innerHTML;
-
-    OnInputChanged();
-}
-
-function OnSelect() {
-    console.log("select");
-
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-
-    // Get the parent element of the selected text
-    const parentElement = range.commonAncestorContainer;
-
-    let clickedColor = null;
-
-    // Traverse up the DOM tree to find the nearest <font> tag
-    let currentNode = parentElement;
-    while (currentNode && currentNode !== input) {
-        if (currentNode.tagName && currentNode.tagName.toLowerCase() === 'font') {
-            clickedColor = currentNode.getAttribute('color');
-        }
-        currentNode = currentNode.parentNode;
-    }
-
-    if (clickedColor == null) {
-        clickedColor = "#" + defaultColor;
-    }
-
-
-    color.value = clickedColor;
-}
-
-function OnColorChanged() {
-    const selectedColor = color.value;
-    document.execCommand('foreColor', false, selectedColor);
-
-    OnInputChanged();
-    OnSelect();
-}
-
-
-function OnInputChanged() {
-    let inputValue = input.innerHTML;
-
-    // remove empty color tags
-    inputValue = inputValue.replaceAll(/<font color="#\d{6}">\s*<\/font>/g, "");
-
-    // replace font tags by codes
-    inputValue = inputValue.replaceAll(/<font color="#([a-fA-F0-9]{6})">/g, "[[-][-]$1]");
-
-    // remove closing font tags
-    inputValue = inputValue.replaceAll(/<\/font>/g, "[[-][-]" + defaultColor + "]");
-
-    // remove empty colors
-    inputValue = inputValue.replaceAll(/\[\[-]\[-][a-fA-F0-9]{6}](\s*\[\[-]\[-][a-fA-F0-9]{6}])/g, "$1");
-
-    // remove trailing color
-    inputValue = inputValue.replaceAll(/\[\[-]\[-][a-fA-F0-9]{6}]\s*$/g, "");
-
-    const emojiRegex = new RegExp(Object.keys(emojiToIndex).join("|"), "g");
-
-    let match = null;
-
-    while ((match = emojiRegex.exec(inputValue)) !== null) {
-
-        let replacement = emojiToIndex[match[0]];
-
-        const before = inputValue.substring(0, match.index);
-        const after = inputValue.substring(match.index + match[0].length);
-        const colorMatches = [...before.matchAll(/\[\[-]\[-]([a-fA-F0-9]{6})]/g)];
-        const lastColorMatch = colorMatches[colorMatches.length - 1];
-
-        if (lastColorMatch) {
-            if (lastColorMatch[1] !== defaultColor) {
-                replacement += lastColorMatch[0];
-            }
-        }
-
-
-        inputValue = before + replacement + after;
-    }
-
-    preview.innerHTML = inputValue;
-}
-
-const indexToEmoji = {
-    "[#07]": "😀",
-    "[#08]": "😭",
-    "[#09]": "😌",
-    "[#10]": "😵",
-    "[#11]": "😋",
-    "[#12]": "😂",
-    "[#13]": "😄",
-    "[#14]": "😫",
-    "[#15]": "😁",
-    "[#16]": "😃",
-    "[#17]": "😰",
-    "[#18]": "😢",
-    "[#19]": "😳",
-    "[#20]": "😤",
-    "[#21]": "🌚",
-    "[#22]": "😣",
-    "[#23]": "😮",
-    "[#24]": "😡",
-    "[#25]": "😠",
-    "[#26]": "☹️",
-    "[#27]": "😞",
-    "[#28]": "😝",
-    "[#29]": "😈",
-    "[#30]": "😚",
-    "[#31]": "😘",
-    "[#32]": "😗",
-    "[#33]": "😖",
-    "[#34]": "😕",
-    "[#35]": "😔",
-    "[#36]": "😇",
-    "[#37]": "😴",
-    "[#38]": "😓",
-    "[#39]": "😒",
-    "[#40]": "😑",
-    "[#41]": "🔥",
-    "[#42]": "😏",
-    "[#43]": "😎",
-    "[#44]": "😍",
-    "[#50]": "😊",
-    "[#52]": "☺️",
-    "[#53]": "😉",
-    "[#54]": "😜",
-    "[#55]": "😨",
-    "[#56]": "😱",
-    "[#57]": "😪",
-    "[#58]": "😷",
-    "[#62]": "👍",
-    "[#63]": "👎",
-    "[#64]": "👌",
-    "[#65]": "👊",
-    "[#66]": "✊",
-    "[#67]": "✌️",
-    "[#68]": "👋",
-    "[#69]": "✋",
-    "[#70]": "👐",
-    "[#71]": "👇",
-    "[#72]": "👆",
-    "[#73]": "👉",
-    "[#74]": "👈",
-    "[#75]": "🙌",
-    "[#76]": "🙏",
-    "[#77]": "☝️",
-    "[#78]": "👏",
-    "[#79]": "💪",
-    "[#01]": "💦",
-    "[#02]": "💥",
-    "[#03]": "💔",
-    "[#04]": "🍻",
-    "[#05]": "🌹",
-    "[#06]": "❤️",
-    "[#45]": "💋",
-    "[#46]": "✨",
-    "[#47]": "🎂",
-    "[#48]": "❓",
-    "[#49]": "🐷",
-    "[#59]": "💤",
-    "[#60]": "🎵",
-    "[#61]": "💩",
-    "[#80]": "👩‍❤️‍👨",
-    "[#81]": "👩‍❤️‍💋‍👨",
-    "[#83]": "👀",
-    "[#84]": "🥚",
-}
-
 const emojiToIndex = {
     "😀": "[#07]",
     "😭": "[#08]",
@@ -269,6 +89,101 @@ const emojiToIndex = {
     "👀": "[#83]",
     "🥚": "[#84]"
 }
+
+function Init() {
+    input.innerHTML = exampleTemplate;
+
+    color.value = "#" + defaultColor;
+
+    OnInputChanged();
+}
+
+function SetTemplate() {
+    document.getElementById("input").innerHTML = event.currentTarget.innerHTML;
+
+    OnInputChanged();
+}
+
+function OnSelect() {
+    console.log("select");
+
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+
+    // Get the parent element of the selected text
+    const parentElement = range.commonAncestorContainer;
+
+    let clickedColor = null;
+
+    // Traverse up the DOM tree to find the nearest <font> tag
+    let currentNode = parentElement;
+    while (currentNode && currentNode !== input) {
+        if (currentNode.tagName && currentNode.tagName.toLowerCase() === 'font') {
+            clickedColor = currentNode.getAttribute('color');
+        }
+        currentNode = currentNode.parentNode;
+    }
+
+    if (clickedColor == null) {
+        clickedColor = "#" + defaultColor;
+    }
+
+
+    color.value = clickedColor;
+}
+
+function OnColorChanged() {
+    const selectedColor = color.value;
+    document.execCommand('foreColor', false, selectedColor);
+
+    OnInputChanged();
+    OnSelect();
+}
+
+function OnInputChanged() {
+    let inputValue = input.innerHTML;
+
+    // remove empty color tags
+    inputValue = inputValue.replaceAll(/<font color="#\d{6}">\s*<\/font>/g, "");
+
+    // replace font tags by codes
+    inputValue = inputValue.replaceAll(/<font color="#([a-fA-F0-9]{6})">/g, "[[-][-]$1]");
+
+    // remove closing font tags
+    inputValue = inputValue.replaceAll(/<\/font>/g, "[[-][-]" + defaultColor + "]");
+
+    // remove empty colors
+    inputValue = inputValue.replaceAll(/\[\[-]\[-][a-fA-F0-9]{6}](\s*\[\[-]\[-][a-fA-F0-9]{6}])/g, "$1");
+
+    // remove trailing color
+    inputValue = inputValue.replaceAll(/\[\[-]\[-][a-fA-F0-9]{6}]\s*$/g, "");
+
+    const emojiRegex = new RegExp(Object.keys(emojiToIndex).join("|"), "g");
+
+    let match = null;
+
+    while ((match = emojiRegex.exec(inputValue)) !== null) {
+
+        let replacement = emojiToIndex[match[0]];
+
+        const before = inputValue.substring(0, match.index);
+        const after = inputValue.substring(match.index + match[0].length);
+        const colorMatches = [...before.matchAll(/\[\[-]\[-]([a-fA-F0-9]{6})]/g)];
+        const lastColorMatch = colorMatches[colorMatches.length - 1];
+
+        if (lastColorMatch) {
+            if (lastColorMatch[1] !== defaultColor) {
+                replacement += lastColorMatch[0];
+            }
+        }
+
+
+        inputValue = before + replacement + after;
+    }
+
+    preview.innerHTML = inputValue;
+}
+
 
 
 function Insert() {
---
+++ test2/functions.js
@@ -0,0 +1,118 @@
+const input = document.getElementById("input");
+const colorPattern = /\[\[-]\[-]([a-fA-F0-9]{6})]/g;
+const colorWithoutTextPattern = /(\[\[-]\[-][a-fA-F0-9]{6}])(?=\s*\[#\d{2}]|\s*\[\[-]\[-][a-fA-F0-9]{6})/g;
+const indexToEmoji = {
+    "[#07]": "😀",
+    "[#08]": "😭",
+    "[#09]": "😌",
+    "[#10]": "😵",
+    "[#11]": "😋",
+    "[#12]": "😂",
+    "[#13]": "😄",
+    "[#14]": "😫",
+    "[#15]": "😁",
+    "[#16]": "😃",
+    "[#17]": "😰",
+    "[#18]": "😢",
+    "[#19]": "😳",
+    "[#20]": "😤",
+    "[#21]": "🌚",
+    "[#22]": "😣",
+    "[#23]": "😮",
+    "[#24]": "😡",
+    "[#25]": "😠",
+    "[#26]": "☹️",
+    "[#27]": "😞",
+    "[#28]": "😝",
+    "[#29]": "😈",
+    "[#30]": "😚",
+    "[#31]": "😘",
+    "[#32]": "😗",
+    "[#33]": "😖",
+    "[#34]": "😕",
+    "[#35]": "😔",
+    "[#36]": "😇",
+    "[#37]": "😴",
+    "[#38]": "😓",
+    "[#39]": "😒",
+    "[#40]": "😑",
+    "[#41]": "🔥",
+    "[#42]": "😏",
+    "[#43]": "😎",
+    "[#44]": "😍",
+    "[#50]": "😊",
+    "[#52]": "☺️",
+    "[#53]": "😉",
+    "[#54]": "😜",
+    "[#55]": "😨",
+    "[#56]": "😱",
+    "[#57]": "😪",
+    "[#58]": "😷",
+    "[#62]": "👍",
+    "[#63]": "👎",
+    "[#64]": "👌",
+    "[#65]": "👊",
+    "[#66]": "✊",
+    "[#67]": "✌️",
+    "[#68]": "👋",
+    "[#69]": "✋",
+    "[#70]": "👐",
+    "[#71]": "👇",
+    "[#72]": "👆",
+    "[#73]": "👉",
+    "[#74]": "👈",
+    "[#75]": "🙌",
+    "[#76]": "🙏",
+    "[#77]": "☝️",
+    "[#78]": "👏",
+    "[#79]": "💪",
+    "[#01]": "💦",
+    "[#02]": "💥",
+    "[#03]": "💔",
+    "[#04]": "🍻",
+    "[#05]": "🌹",
+    "[#06]": "❤️",
+    "[#45]": "💋",
+    "[#46]": "✨",
+    "[#47]": "🎂",
+    "[#48]": "❓",
+    "[#49]": "🐷",
+    "[#59]": "💤",
+    "[#60]": "🎵",
+    "[#61]": "💩",
+    "[#80]": "👩‍❤️‍👨",
+    "[#81]": "👩‍❤️‍💋‍👨",
+    "[#83]": "👀",
+    "[#84]": "🥚",
+}
+const defaultColor = "ffffc8";
+
+function OnInputChanged() {
+    let inputValue = input.value;
+    inputValue = inputValue.replaceAll(colorWithoutTextPattern, "");
+
+    const emojiRegex = new RegExp(Object.keys(indexToEmoji).join("|").replaceAll("[", "\\["), "g");
+    
+    // replacing emoji indices with their emoji
+    let match = null;
+    while ((match = emojiRegex.exec(inputValue)) !== null) {
+        let replacement = indexToEmoji[match[0]];
+        const before = inputValue.substring(0, match.index);
+        const after = inputValue.substring(match.index + match[0].length);
+        const colorMatches = [...before.matchAll(colorPattern)];
+        const lastColorMatch = colorMatches[colorMatches.length - 1];
+        if(lastColorMatch != null){
+            if(lastColorMatch[1] !== defaultColor){
+                replacement = "</span>" + replacement;
+            }
+        }
+        
+        inputValue = before + replacement + after;
+    }
+
+    const preview = document.getElementById("preview");
+
+    // replacing color tags
+    preview.innerHTML = inputValue.replace(/\[\[-]\[-]([0-9A-Fa-f]{6})]/g, "<span style='color:#$1'>");
+}
+
